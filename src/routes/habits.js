29d9925991059f5f24ef89f1c5fdf40601e4eb67
zXAZURIANXz <zxazurianxz@MacBook-Pro-de-zXAZURIANXz.local>
2025-07-08 13:34:00 -0600

    HABIT-BE-007 create endpoint to mark habit as complete

--- src/routes/habits.js
+++ src/routes/habits.js
@@ -15,22 +15,41 @@ router.post('/', async (req, res) => {
     const newHabit = new UserHabit({ title, description, color, isCompleted, icon });
     await newHabit.save();
     res.status(201).json(newHabit);
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({ error: 'Error al crear hábito' });
   }
 });
 
+
+/* Get all habits */
 router.get('/', async (req,res) => {
 	try{
 		const data = await UserHabit.find();
 		res.status(200).json(data);
-	}catch (err){
+	}catch (error){
 		res.status(500).json({message:'Error fetching data', error})
 	}
 })
 
-router.get('/', (req, res) => {
-  res.send('Funciona la ruta /habits')
+
+/* Mark Habit like complete by day */
+router.post('/complete', async (req,res) => {
+	const { isCompleted, date, _idHabit } = req.body;
+	try{
+		const data = await UserHabit.findByIdAndUpdate(
+			_idHabit,
+			{
+				$set: { isCompleted: isCompleted },
+				$push: { completedDates: date }
+			},
+			{ new: true }
+		);
+		res.status(200).json(data);
+	}catch (error){
+		res.status(500).json({message:'Error fetching data', error})
+	}
 })
+
+
 
 module.exports = router;
