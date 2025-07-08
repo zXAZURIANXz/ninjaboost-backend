cd2b30d299c587578cae6fd02a64d06ec8bec94f
zXAZURIANXz <zxazurianxz@MacBook-Pro-de-zXAZURIANXz.local>
2025-07-08 12:56:53 -0600

    HABIT-BE-006 adding new array with completed Dates

--- src/models/UserHabit.js
+++ src/models/UserHabit.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const UserHabitSchema = new mongoose.Schema({
 
-		title       : {type: String, required: true },
-    description : {type: String },
-    color       : {type: String },
-    isCompleted : {type: Boolean, required: true },
-    icon        : {type: String },
+		title       	 : {type: String, required: true },
+    description 	 : {type: String },
+    color       	 : {type: String },
+    isCompleted    : {type: Boolean, required: true },
+		completedDates : {type: Array },
+    icon         	 : {type: String },
 
 },{ timestamps: true })
 
